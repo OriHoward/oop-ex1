e680d9b4ef326a7f70247ab3b0c18e78f1035d53
OriHoward <ori.howard@gmail.com>
2021-11-12 03:30:50 +0200

    fixed bug in utils function - qualified calls

--- AlgoUtils.py
+++ AlgoUtils.py
@@ -15,9 +15,9 @@ def add_qualified_calls(allocated_call: CallForElevator, calls: list[CallForElev
     interval_to_check = elev.get_call_endtime(allocated_call)
     for call in calls:
         if allocated_call.time < call.time < interval_to_check and call.curr_allocation != -1 and call.call_direction == allocated_call.call_direction:
-            if call.call_direction == StatusEnum.UP and allocated_call.source < call.source:
+            if call.call_direction == StatusEnum.UP and allocated_call.source < call.source < allocated_call.dest:
                 qualified_calls.append(call)
-            if call.call_direction == StatusEnum.DOWN and allocated_call.source > call.source:
+            if call.call_direction == StatusEnum.DOWN and allocated_call.source > call.source > allocated_call.dest:
                 qualified_calls.append(call)
     qualified_calls = filter_to_qualified_calls(allocated_call, qualified_calls, elev)
     return qualified_calls
