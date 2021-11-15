a0b9c4ea9cb633d45cc22d591226859717021044
EitanKats <eitan.kats96@gmail.com>
2021-11-15 20:46:03 +0200

    added additional information in the file parser

--- input_parser.py
+++ input_parser.py
@@ -6,9 +6,9 @@ from CallForElevator import CallForElevator
 from Elevator import Elevator
 
 
-def parse_input_csv(max_floor, min_floor, calls_file):
+def parse_input_csv(max_floor: int, min_floor: int, calls_file_path: str) -> list[CallForElevator]:
     parsed_calls = []
-    with open(calls_file) as f:
+    with open(calls_file_path) as f:
         csv_reader = csv.reader(f)
         for row in csv_reader:
             prefix, time, source, dest, status, curr_allocation = row
@@ -18,9 +18,14 @@ def parse_input_csv(max_floor, min_floor, calls_file):
     return parsed_calls
 
 
-def parse_input_building(building_file):
+def parse_input_building(building_file_path: str) -> Building:
+    """
+    parses a json file the contains the information about the building
+    :param building_file_path:
+    :return:
+    """
     parsed_elevators = []
-    with open(building_file) as f:
+    with open(building_file_path) as f:
         data = json.load(f)
 
     for idx, elev in enumerate(data["_elevators"]):
