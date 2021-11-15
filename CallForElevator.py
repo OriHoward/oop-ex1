be7ff72e63f1b6b5875aebe0dde2b440cdc9264b
EitanKats <eitan.kats96@gmail.com>
2021-11-15 14:57:54 +0200

    added units tests to check if calls parsing works as expected

--- CallForElevator.py
+++ CallForElevator.py
@@ -13,6 +13,10 @@ class CallForElevator:
     def __str__(self):
         return f"Elevator call,{self.time},{self.source},{self.dest},{self.status},{self.curr_allocation}"
 
+    def __eq__(self, other):
+        return self.source == other.source and \
+               self.dest == other.dest and \
+               self.curr_allocation == other.curr_allocation and self.status == other.status
+
     def to_csv_array(self):
         return ["Elevator call", self.time, self.source, self.dest, self.status.value, self.curr_allocation]
-
--- tests/test_input_parser.py
+++ tests/test_input_parser.py
@@ -1,16 +1,17 @@
 from unittest import TestCase
 from unittest.mock import patch, mock_open
 
+from CallForElevator import CallForElevator
 from input_parser import parse_input_csv
 
 
 class Test(TestCase):
     def test_parse_input_csv(self):
-        with patch("builtins.open", mock_open(read_data="Elevator call,4.993816266,-6,78,0,-1")) as mock_file:
-            assert open("path/to/open").read() == "Elevator call,4.993816266,-6,78,0,-1"
-            parse_input_csv(-10, 90, "path/to/open")
-            mock_file.assert_called_with("path/to/open")
-        self.assertEqual('foo'.upper(), 'FOO')
+        with patch("builtins.open", mock_open(read_data="Elevator call,4.993816266,-6,78,0,-1")):
+            calls = parse_input_csv(90, -10, "path/to/open")
+            filtered_calls = parse_input_csv(50, -10, "path/to/open")
+        self.assertEqual(calls, [CallForElevator('4.993816266', '-6', '78', '0', '-1')])
+        self.assertEqual(filtered_calls, [])
 
     def test_parse_input_building(self):
         self.assertEqual('foo'.upper(), 'FOO')
