d3167a158de0ffabfe9d4a8d90f78dc81c0dfe48
EitanKats <eitan.kats96@gmail.com>
2021-11-16 18:10:42 +0200

    added a few additional tests

--- tests/test_AlgoUtils.py
+++ tests/test_AlgoUtils.py
@@ -15,5 +15,11 @@ class Test(TestCase):
         self.assertEqual(filtered_calls, calls)
 
     def test_filter_qualified_calls(self):
-        AlgoUtils.filter_qualified_calls()
-        self.fail()
+        curr_elev = Elevator(0, '1', '-10', '100', 2, 2, 3, 3)
+        calls = [CallForElevator('8', '0', '25', '0', '-1'),
+                 CallForElevator('11.0', '5', '28', '0', '-1'),
+                 CallForElevator('18', '3', '78', '0', '-1')]
+        filtered_calls = AlgoUtils.filter_qualified_calls(CallForElevator('5', '-6', '46', '0', '-1'), calls, curr_elev)
+        del calls[-1]
+        self.assertEqual(filtered_calls, calls)
+        # self.fail()
